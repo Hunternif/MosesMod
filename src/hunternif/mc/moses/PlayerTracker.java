84c69a3805d2b546b9190ac3f64c6e8330e28fd0
Hunternif <hunternif@gmail.com>
2013-05-26 22:08:16 +0400

    removed outdated TODO

--- src/hunternif/mc/moses/PlayerTracker.java
+++ src/hunternif/mc/moses/PlayerTracker.java
@@ -12,7 +12,6 @@ public class PlayerTracker implements IPlayerTracker {
 
 	@Override
 	public void onPlayerLogout(EntityPlayer player) {
-		// TODO close all of this player's passages
 		if (!player.worldObj.isRemote) {
 			((StaffOfMoses)MosesMod.staffOfMoses).restoreWater(player.worldObj, player);
 		}
