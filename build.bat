ac9fad9f0b3aa5db2fe57ff4f7afb1a2f6fad435
Hunternif <hunternif@gmail.com>
2013-05-29 01:50:32 +0400

    Added pseudo-automated batch build file
    It allows to keep the source files outside the Forge/mcp/src folder,
    which is useful for developing and versioning of several mods. It
    requires that there is FORGE_HOME environment variable, and if WinRAR is
    installed in the default location, it will perform the archiving into
    the final file as well.

---
+++ build.bat
@@ -0,0 +1,44 @@
+@echo off
+if "%FORGE_HOME%" == "" goto forgeNotSet
+
+echo == Copying "hunternif" ==
+xcopy src\hunternif /s /y "%FORGE_HOME%\mcp\src\minecraft\hunternif\"
+echo == Copying "mods" ==
+xcopy src\mods /s /y "%FORGE_HOME%\mcp\src\minecraft\mods\"
+
+set MOD_SRC_DIR=%cd%
+cd "%FORGE_HOME%\mcp"
+runtime\bin\python\python_mcp runtime\recompile.py %*
+runtime\bin\python\python_mcp runtime\reobfuscate.py %*
+
+echo == Removing "hunternif" ==
+rmdir "%FORGE_HOME%\mcp\src\minecraft\hunternif"
+echo == Removing "mods" ==
+rmdir "%FORGE_HOME%\mcp\src\minecraft\mods"
+
+set WINRAR=C:\Program Files\WinRAR\Rar.exe
+if not exist "%WINRAR%" goto noWinRAR
+echo Found WinRAR
+cd "%FORGE_HOME%\mcp\reobf\minecraft"
+echo == Copying resource files to "reobf" ==
+xcopy "%MOD_SRC_DIR%\src\mods" /s /y "%FORGE_HOME%\mcp\reobf\minecraft\mods\"
+xcopy "%MOD_SRC_DIR%\src\mcmod.info" /y "%FORGE_HOME%\mcp\reobf\minecraft\"
+xcopy "%MOD_SRC_DIR%\src\*.png" /y "%FORGE_HOME%\mcp\reobf\minecraft\"
+echo == Creating achive ==
+rem TODO retreive the mod name and version
+"%WINRAR%" a -r -ep1 mod.zip ./*
+pause
+goto end
+
+:noWinRAR
+echo WinRAR was not found on your computer, you will have to manually archive the contents of "%FORGE_HOME%\mcp\reobf\minecraft" and add to the archive the "mods" folder, the file "mcmod.info" and the picture.
+pause
+goto end
+
+:forgeNotSet
+echo FORGE_HOME is not set
+pause
+goto end
+
+
+:end
