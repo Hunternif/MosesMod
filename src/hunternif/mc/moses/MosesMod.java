f3e1665ff5eab4b286bc252b8f796850fa4923aa
Hunternif <hunternif@gmail.com>
2013-09-29 18:59:00 +0400

    Updated build.bat, description and bumped version to 1.3

--- src/hunternif/mc/moses/MosesMod.java
+++ src/hunternif/mc/moses/MosesMod.java
@@ -48,7 +48,7 @@ import cpw.mods.fml.relauncher.Side;
 public class MosesMod {
 	public static final String ID = "MosesMod";
 	public static final String NAME = "Moses Mod";
-	public static final String VERSION = "1.2";
+	public static final String VERSION = "1.3";
 	public static final String CHANNEL = ID;
 	
 	public static final String KEY_PASSAGE_HALF_WIDTH = "mosesPassageHalfWidth";
